7c5e5f65e49998d9d850ca1159ee4810dfccb321
Sean-Andrew-Stanek <sean.andrew.stanek@gmail.com>
2023-10-01 12:11:08 +0900

    Exercise 1.5 changes
    - Changed the dataset to IIFE
      - Added return and add functions
    - Changed the for loop into a foreach
    - Added an example of adding to dataset

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
-//Exercise 1.3
+//Exercise 1.5
 
 document.write ("<center><h1>Exercise 1.3</h1> <hr>");
 
 
-// Our Pokedex
-let pokemonList = [
-    /* Base Object
-    {
-    name: '',
-    type: []
-    size:
-    }
-    */
-    {
+/*** EXERCISE 1.5 IIFE ***/
+let pokemonRepository = function() {
+
+    //*** EXERCISE 1.5 DATASET ***/
+    let pokemonList = [{
         name: 'Bulbasaur',
         type: ['Grass', 'Poison'],
         size: 1
@@ -29,56 +24,67 @@ let pokemonList = [
     },
     {
         name: 'Eevee',
-        type: ['Normal'],
+        type: ['Normal',''],
         size: 1
-    }
-]
+    }];
 
+    return{
+        /*** EXERCISE 1.5 PUBLIC FUNCTIONS ***/
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+}();
+
+//BONUS: shows that you can add to the list
+let addPokemon = {name: 'Charmander', type: ['Fire',''], size: 1};
+pokemonRepository.add(addPokemon);
 //Table Style
 document.write('<style> table, th, td { border: 1px solid white; } </style>');
 
 //Initialize table DO NOT REMOVE
 document.write('<table>');
 
-if(pokemonList.length>0){
-    //Table Header
-    document.write('<tr style="border: white 5px solid"><th>Pokemon Name</th><th>Type One</th><th>Type Two</th><th>Size</th></tr>');
+/*** EXERCISE 1.5 FOREACH LOOP ***/
+pokemonRepository.getAll().forEach(function(pokemon)
+{
+    //DO NOT REMOVE
+    document.write('<tr>')
 
-    for(let i = 0; i<pokemonList.length; i++)
-    {
-        //DO NOT REMOVE
-        document.write('<tr>')
+    //Name
+    document.write('<td>'+ pokemon.name + '</td>')
 
-        //Name
-        document.write('<td>'+ pokemonList[i].name + '</td>')
+    //Type One (expects there to be data)
+    document.write('<td>' + pokemon.type[0]+'</td>')
 
-        //Type One (expects there to be data)
-        document.write('<td>' + pokemonList[i].type[0]+'</td>')
+    //Prints second type or an empty cell if there isn't one
+    if(pokemon.type.length>1)
+        document.write('<td>' + pokemon.type[1]+'</td>')
+    else
+        document.write('<td></td>')
+    
+    //Size  (will be deleted at first opportunity)
+    document.write('<td>');
+    switch(pokemon.size) {
+        case 1:
+            document.write('Small (unevolved)');
+            break;
+        case 2:
+            document.write('Medium (first evolution)');
+            break;
+        default:
+            document.write('Large (Wow, that\'s Big!)')
+    };
+    document.write('</td>');
 
-        //Prints second type or an empty cell if there isn't one
-        if(pokemonList[i].type.length>1)
-            document.write('<td>' + pokemonList[i].type[1]+'</td>')
-        else
-            document.write('<td></td>')
-        
-        //Size  (will be deleted at first opportunity)
-        document.write('<td>');
-        switch(pokemonList[i].size) {
-            case 1:
-                document.write('Small (unevolved)');
-                break;
-            case 2:
-                document.write('Medium (first evolution)');
-                break;
-            default:
-                document.write('Large (Wow, that\'s Big!)')
-        };
-        document.write('</td>');
+    //Ends row DO NOT REMOVE
+    document.write('</tr>');
 
-        //Ends row DO NOT REMOVE
-        document.write('</tr>');
-    }
-}
+});
+
 
 //End Table DO NOT REMOVE
 document.write('</table>');
