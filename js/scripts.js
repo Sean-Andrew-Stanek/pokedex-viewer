cdf97442783cdb529b17d557bfd0bdf977eb7ffe
Sean-Andrew-Stanek <sean.andrew.stanek@gmail.com>
2023-10-01 09:41:30 +0900

    Exercise 1.3
    - Added comments for readability
    - Added a column for size
    - Added a switch for size data
    - Centered data (readability)
    - Removed Exercise 1.1 content

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,87 @@
-//Exercise 1.1
+//Exercise 1.3
 
-alert('Hello world');
-document.write("Exercise 1.1 <hr>")
-let favoriteFood = ['pickles', 'potatoes', 'carrots'];
-document.write('My favorite food');
+document.write ("<center><h1>Exercise 1.3</h1> <hr>");
 
-if(favoriteFood.length>0){
-    if(favoriteFood.length>1){
-    document.write('s are ');
-    for (var i = 0; i<favoriteFood.length-1; i++)
-        document.write(favoriteFood[i] + ', ');
-    document.write('and ');
-    }
-    else{
-        document.write(' is ');
-    }
 
-    document.write(favoriteFood[favoriteFood.length-1] + ".");
-}else{
-    document.write('I don\'t like food.  It\'s yucky!');
-}
-
-//Exercise 1.2
-
-document.write ("<hr>Exercise 1.2 <hr>")
-
+// Our Pokedex
 let pokemonList = [
     /* Base Object
     {
     name: '',
     type: []
+    size:
     }
     */
     {
         name: 'Bulbasaur',
-        type: ['Grass', 'Poison']
+        type: ['Grass', 'Poison'],
+        size: 1
     },
     {
         name: 'Ivysaur',
-        type: ['Grass', 'Poison']
+        type: ['Grass', 'Poison'],
+        size: 2
     },
     {
         name: 'Venusaur',
-        type: ['Grass', 'Poison']
+        type: ['Grass', 'Poison'],
+        size: 3
     },
     {
         name: 'Eevee',
-        type: ['Normal']
+        type: ['Normal'],
+        size: 1
     }
 ]
 
+//Table Style
 document.write('<style> table, th, td { border: 1px solid white; } </style>');
+
+//Initialize table DO NOT REMOVE
 document.write('<table>');
 
 if(pokemonList.length>0){
-    document.write('<tr style="border: white 5px solid"><th>Pokemon Name</th><th>Type One</th><th>Type Two</th></tr>');
+    //Table Header
+    document.write('<tr style="border: white 5px solid"><th>Pokemon Name</th><th>Type One</th><th>Type Two</th><th>Size</th></tr>');
 
     for(let i = 0; i<pokemonList.length; i++)
     {
+        //DO NOT REMOVE
         document.write('<tr>')
+
+        //Name
         document.write('<td>'+ pokemonList[i].name + '</td>')
-        //Assumes there is at least one type defined
+
+        //Type One (expects there to be data)
         document.write('<td>' + pokemonList[i].type[0]+'</td>')
+
+        //Prints second type or an empty cell if there isn't one
         if(pokemonList[i].type.length>1)
             document.write('<td>' + pokemonList[i].type[1]+'</td>')
+        else
+            document.write('<td></td>')
+        
+        //Size  (will be deleted at first opportunity)
+        document.write('<td>');
+        switch(pokemonList[i].size) {
+            case 1:
+                document.write('Small (unevolved)');
+                break;
+            case 2:
+                document.write('Medium (first evolution)');
+                break;
+            default:
+                document.write('Large (Wow, that\'s Big!)')
+        };
+        document.write('</td>');
+
+        //Ends row DO NOT REMOVE
         document.write('</tr>');
     }
 }
 
+//End Table DO NOT REMOVE
 document.write('</table>');
+
+//Remove center from top
+document.write('</center>')
