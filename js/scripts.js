f6b76efb55cba523213f3bb3e1b3e91fd97b0da2
elyikiam <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-10-05 01:51:13 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let pokemonRepository = (function() {
     
     let pokemonList = [];
-    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=50';
 
     //style.css button types  TODO:  Add all types
     let implementedTypes = ['normal', 'fire', 'water', 'grass'];
@@ -15,8 +15,13 @@ let pokemonRepository = (function() {
     function add(pokemon){
         if (
             typeof pokemon === 'object' &&
-            'name' in pokemon
-        ) {
+            'name' in pokemon &&
+            'detailsURL' in pokemon &&
+            'imageURL' in pokemon &&
+            'height' in pokemon &&
+            'types' in pokemon &&
+            'id' in pokemon
+        ){
             pokemonList.push(pokemon);
         } else {
             console.log('pokemon formatting error');
@@ -27,85 +32,71 @@ let pokemonRepository = (function() {
         return pokemonList;
     }
 
-    function loadDetails(item) {
+    async function loadDetails(item) {
         let url = item.detailsURL;
-        
-        return fetch(url)
-        .then(function (response){
-            //return JSON to next function
-            return response.json();
-        }).then(function (details) {
+        try {
+            const response = await fetch(url);
+            const details = await response.json();
             //TODO:  Don't modify the parameter, but return an array and modify where called
+            let returnObject = {};
             item.imageURL = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
             item.id = details.id;
-        }).catch(function (error) {
+        } catch (error) {
             console.error(error);
-        })
+        }
     }
-dfdfgdsfg
-    function loadList() {
-        return fetch(apiURL)
-        .then(function (response) {
-            //send the json to next function
-            return response.json();
-        }).then(function (json) {
-            json.results.forEach(function(item) {
+
+    async function loadList() {
+        //Get stream from URL
+        try {
+            const response = await fetch(apiURL);
+            const json = await response.json();
+            //Create the pokemon object
+            json.results.forEach(async function (item) {
                 let pokemon = {
                     //Button display name; capitalized the way it should be.
-                    name: (item.name.slice(0,1)).toUpperCase()+item.name.slice(1),
+                    name: (item.name.slice(0, 1)).toUpperCase() + item.name.slice(1),
                     //Where we find more data
                     detailsURL: item.url
                 };
+                await loadDetails(pokemon);                
                 add(pokemon);
-            });            
-        }).catch(function(error)
-        {
+            });
+        } catch (error) {
             console.error(error);
-        })
+        }
     }
 
     function addListItem(pokemon) {
+        // Left side "button container"
+        let selectablePokemonList = document.querySelector('.selectable-pokemon-list');
+
         // Create a list item    
         let listItem = document.createElement('div');
 
         let button = document.createElement('button');
         button.innerText = pokemon.name;
         
-        /* console.log(pokemon); */
+        //If the type colors for buttons have been added in style.css, give the type as a class
+        //otherwise, give it default colors
+        let pokemonType = pokemon.types[0].type.name.toLowerCase();
+        if(implementedTypes.includes(pokemonType))
+            button.classList.add(pokemonType);
+        else
+            button.classList.add('default-type')
 
-        //I need both the ID and the color for the buttons / order, so we are loading the data now.
-        //This causes delay, but later, we can hide the delay through visual trickery.
-        loadDetails(pokemon)
-        .then(function()
-        {    
-            /* console.log(pokemon); */
-            //If the type colors for buttons have been added in style.css, give the type as a class
-            //otherwise, give it default colors
-            let pokemonType = pokemon.types[0].type.name.toLowerCase();
-            if(implementedTypes.includes(pokemonType))
-                button.classList.add(pokemonType);
-            else
-                button.classList.add('default-type')
-
-            button.classList.add('pokemon-button');
-            button.addEventListener('click', function() {
-                pokemonRepository.showDetails(pokemon);
-            });
-            listItem.appendChild(button);
-            selectablePokemonList.appendChild(listItem);
-        }).catch(function(error)
-        {
-            console.error(error)
-        }).finally(function(){
-            console.log(pokemon);
-        })
+        button.classList.add('pokemon-button');
+        button.addEventListener('click', function() {
+            pokemonRepository.showDetails(pokemon);
+        });
+        listItem.appendChild(button);
+        selectablePokemonList.appendChild(listItem);
     }
 
 
     return {
-        add: add,
         getAll: getAll,
         loadList: loadList,
         addListItem: addListItem,
@@ -113,20 +104,17 @@ dfdfgdsfg
     };
 })();
 
-// Left side "button container"
-let selectablePokemonList = document.querySelector('.selectable-pokemon-list');
-
-//Create buttons for the repository
-pokemonRepository.loadList()
-.then(function(){
+async function createButtons(){
+    //Create buttons for the repository
+    await pokemonRepository.loadList();
+    
+    /*****   Look here  *****/
+    console.log(pokemonRepository.getAll());
     pokemonRepository.getAll().forEach(function(pokemon)
     {
+        console.log("THIS DOESN'T FIRE");
         pokemonRepository.addListItem(pokemon);
     });
-}).catch(function(error)
-{
-    console.error(error);
-}).finally(function(){
-    console.log(pokemonRepository.getAll()[0]);
-});
+}
 
+createButtons();
